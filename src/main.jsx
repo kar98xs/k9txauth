a7f4a1f155a82b1d6c1562001b1cda8f481cbe6c
kar98xs <k9txss@gmail.com>
2026-02-26 17:34:52 +0530

    Import suppressConsole utility in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./utils/suppressConsole";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -11,6 +12,6 @@ createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <App />
-  </GoogleOAuthProvider>
+  </GoogleOAuthProvider>,
   // </StrictMode>
 );
